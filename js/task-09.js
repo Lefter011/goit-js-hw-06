0a17870701b8cce2110536137cac132bcfd6c952
Mike Chernetskyi <michael.chernetskyi@gmail.com>
2022-09-10 16:03:16 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+
+const colorLabelRef = document.querySelector('.color');
+const btnChangerRef = document.querySelector('.change-color');
+
+btnChangerRef.addEventListener('click', (event) => {
+    colorLabelRef.textContent = document.body.style.backgroundColor = getRandomHexColor();
+    document.body.style.transition = '500ms';
+})
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
