ff53ea1ff28fd348f1f32b495f6d19aa78831370
Mike Chernetskyi <michael.chernetskyi@gmail.com>
2022-09-10 15:39:39 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector('.login-form');
+const formValue = {};
+const submitBtnRef = formRef.querySelector('button[type="submit"]');
 
+formRef.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert('Все поля должны быть заполнены!');
+    } 
+
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+        formValue[name] = value;
+    })
+    console.log(formValue);
+    formRef.reset();
+})
